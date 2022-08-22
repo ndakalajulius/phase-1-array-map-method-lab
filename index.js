863d024986f746b19cd58e47e3aaad7c2d4aedf6
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-22 08:32:12 +0300

    complted assignment

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+return tutorials.map(function(tutorials){
+  return tutorials.split(' ').map(function (stringSentence){
+    return stringSentence.toUpperCase().charAt(0) + stringSentence.slice(1)}).join(' ')
+  });
+};
+
+
